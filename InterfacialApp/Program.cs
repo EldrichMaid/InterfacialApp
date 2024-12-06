82560bf86b3fc77ed5798f45bf2ae8153e07c839
EldrichMaid <kupo@mail.ru>
2024-12-06 14:04:55 +0300

    Добавьте файлы проекта.

---
+++ InterfacialApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace InterfacialApp
+{
+    public interface Ilogger
+    {
+        void Event(string message);
+        void Error(string message);
+    }
+
+    public class Logger : Ilogger
+    {
+        public void Error(string message)
+        {
+           Console.WriteLine(message); 
+        }
+
+        public void Event(string message)
+        {
+            //отправка в бд
+            //отправка на почту
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine(message);
+        }
+    }
+
+    public interface IWorker
+    {
+        void RunWorker();
+    }
+
+    internal class Program
+    {
+        static Ilogger logger {  get; set; }
+        static void Main()
+        {
+           logger = new Logger();
+
+            var worker1 = new Worker1(logger);
+            var worker2 = new Worker2(logger);
+            var worker3 = new Worker3(logger);
+
+            worker1.RunWorker();
+            worker2.RunWorker();
+            worker3.RunWorker();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ InterfacialApp/Worker1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacialApp
+{
+    public class Worker1: IWorker
+    {
+        Ilogger Logger {get;}
+        public Worker1 (Ilogger logger)
+        {
+            Logger = logger;
+        }
+
+        public void RunWorker()
+        {
+            Logger.Event("Worker 1 event has occured");
+            Thread.Sleep(3000);
+            Logger.Event("Worker 1 event has ended");
+        }
+       
+    }
+
+    public class Worker2 : IWorker
+    {
+        Ilogger Logger { get; }
+        public Worker2(Ilogger logger)
+        {
+            Logger = logger;
+        }
+
+        public void RunWorker()
+        {
+            Logger.Event("Worker 2 event has occured");
+            Thread.Sleep(3000);
+            Logger.Event("Worker 2 event has ended");
+        }
+    }
+
+    public class Worker3 : IWorker
+    {
+        Ilogger Logger { get; }
+        public Worker3(Ilogger logger)
+        {
+            Logger = logger;
+        }
+
+        public void RunWorker()
+        {
+            Logger.Event("Worker 3 event has occured");
+            Thread.Sleep(3000);
+            Logger.Event("Worker 3 event has ended");
+        }
+    }
+
+
+}
